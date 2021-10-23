fee5ba663576e45dc80ba49a115d494f16aff097
Rudy Mollet <91132260+Zenitude@users.noreply.github.com>
2021-10-23 15:37:04 +0200

    Add files via upload

---
+++ Site/scripts/carousel.js
@@ -0,0 +1,26 @@
+/* Carousel */
+const carousel = document.querySelector('.carousel');
+const imagesCarousel = document.querySelectorAll('.carousel img');
+let index = 0;
+
+function carouselAuto()
+{
+    if(index < imagesCarousel.length - 1)
+    {
+        imagesCarousel[index].classList.remove('imageAffiche');
+        imagesCarousel[index].classList.add('imageMasque');
+        index++
+        imagesCarousel[index].classList.remove('imageMasque');
+        imagesCarousel[index].classList.add('imageAffiche');
+    }
+    else if (index === imagesCarousel.length -1)
+    {
+        imagesCarousel[index].classList.remove('imageAffiche');
+        imagesCarousel[index].classList.add('imageMasque');
+        index = 0;
+        imagesCarousel[index].classList.remove('imageMasque');
+        imagesCarousel[index].classList.add('imageAffiche');
+    }
+}
+
+setInterval(carouselAuto, 3000);
